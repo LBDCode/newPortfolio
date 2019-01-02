afa0d20fc889323bd3195d25f4317a68ee877806
LBDCode <libby.duggan@gmail.com>
2019-01-02 15:45:47 -0500

    initial files

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+
+$(".navbar-burger").click(function() {
+
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+});
+
+
+$("#contact").click(function() {
+    $(".modal").toggleClass("is-active");
+});
+
+$(".modal-background").click(function() {
+    $(".modal").toggleClass("is-active");
+});
+
+$(".delete").click(function() {
+    $(".modal").toggleClass("is-active");
+});
+
+$("#close").click(function() {
+    $(".modal").toggleClass("is-active");
+});
+
+
+
